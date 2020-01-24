8f3af2fb9271b1b791e2349aae8560f4821c1143
Chris Sanchez <c.sanch7@gmail.com>
2020-01-24 10:37:44 -0800

    rewrite admin signup process to include error check. 100% coverage.

--- spec/features/admin_signup_pages_spec.rb
+++ spec/features/admin_signup_pages_spec.rb
@@ -4,12 +4,24 @@ describe 'the sign up for an admin account process' do
     it 'signs up an admin account' do
         visit '/'
         click_on 'Sign up'
-        fill_in 'user[name]', with: 'user'
-        fill_in 'user[email]', with: 'user@user.com'
-        fill_in 'user[password]', with: 'user'
-        fill_in 'user[password_confirmation]', with: 'user'
+        fill_in 'user[name]', with: 'admin'
+        fill_in 'user[email]', with: 'admin@admin.com'
+        fill_in 'user[password]', with: 'admin'
+        fill_in 'user[password_confirmation]', with: 'admin'
         page.check('user[admin]')
         click_on 'Sign Up'
         expect(page).to have_content 'Welcome, Admin.'
     end
+
+    it "gives an error when account parameters aren't entered or entered accurately" do
+        visit '/'
+        click_on 'Sign up'
+        fill_in 'user[name]', with: 'admin'
+        fill_in 'user[email]', with: 'admin@admin.com'
+        fill_in 'user[password]', with: 'admin'
+        fill_in 'user[password_confirmation]', with: 'WRONG PASSWORD'
+        page.check('user[admin]')
+        click_on 'Sign Up'
+        expect(page).to have_content "There was a problem signing up."
+      end
 end
