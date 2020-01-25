f5b581a7194705ee60929c7d7669f416e0f06e14
Chris Sanchez <c.sanch7@gmail.com>
2020-01-24 16:20:34 -0800

    100% coverage, 45 passing tests

--- spec/features/add_product_review_pages_spec.rb
+++ spec/features/add_product_review_pages_spec.rb
@@ -19,4 +19,20 @@ describe 'the add a product review process' do
         click_on "Create Review"
         expect(page).to have_content "Michael Bluth"
     end
+
+    it "gives an error when no author is entered" do
+        visit '/'
+        product = FactoryBot.create(:product)
+        user = FactoryBot.create(:user)
+        click_on 'Sign in'
+        fill_in :email, with: 'user@user.com'
+        fill_in :password, with: 'user'
+        click_on 'Log in'
+        expect(page).to have_content "Dishwasher"
+        click_on "Dishwasher"
+        click_on "Add a review"
+
+        click_on "Create Review"
+        expect(page).to have_content "Author can't be blank"
+      end
 end
